20ad88792ca7c7b3e7306b4a068c78eadff6b6e8
alandahmed <110727799+alandahmed@users.noreply.github.com>
2022-08-29 18:39:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+print('Hello')    #Hello
+print("Hi")       #Hi
+print('''Hey''')  #Hey
 
+print('1+1')      #1+1
+print("2+2")      #2+2
+print('''3+3''')  #3+3
+
+print(1+1)        #2
+print(2+2)        #4
+print(3+3)        #6
